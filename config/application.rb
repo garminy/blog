2f1c0ba54d0e067a4e716ec358b75c53fbf6c96a
王若愚 <wrymax@qq.com>
2016-03-15 16:40:35 +0800

    update application config for browserify

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module Blog
     config.react.jsx_transformer_class = React::JSX::JSXTransformer
     config.browserify_rails.commandline_options = "--transform reactify --extension=\".jsx\""
     # config.browserify_rails.commandline_options = '-t coffeeify --extension=".js.coffee"'
+
+
+    # Browserify config
+    config.browserify_rails.paths << /vendor\/assets\/javascripts\/module\.js/
+    config.browserify_rails.source_map_environments << "development"
+    config.browserify_rails.evaluate_node_modules = true
+    config.browserify_rails.force = ->(file) { File.extname(file) == ".ts" }
+    config.browserify_rails.commandline_options = ["-t browserify-shim", "--fast"]
+    config.browserify_rails.commandline_options = "-t browserify-shim --fast"
   end
 end
